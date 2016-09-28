4f399a64dd2960aab7f791ff8e48768ca9688d6d
John Valente <johnv02139@yahoo.com>
2016-09-27 21:33:05 -0700

    Add rc files.

---
+++ rc/profiles.clj
@@ -0,0 +1,7 @@
+;; ln -s ${JV_ENV_HOME}/rc/profiles.clj ~/.lein/profiles.clj
+
+
+{:repl {:plugins [[cider/cider-nrepl "0.13.0"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}
+
+ :user {:plugins [[jonase/eastwood "0.2.3"]] }}
