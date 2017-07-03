3a7df8060383647ff3ada7be52c56c2342e8a640
John Valente <johnv02139@yahoo.com>
2017-07-03 03:19:44 -0700

    Have Cider plugin be part of :user, not :repl
    The Cider docs say make cider-repl a plugin only for the :repl profile, or else the middleware will run all the time and delay startup.  It says you only need it for :repl.  But that's not really true.  If you want to run the project with "lein run" and connect Cider to it, you need to get the middleware plugged in.  The options are to include it in project.clj, or have it more prominent in profiles.clj.  This seems like a better option.

--- rc/profiles.clj
+++ rc/profiles.clj
@@ -1,7 +1,16 @@
 ;; ln -s ${JV_ENV_HOME}/rc/profiles.clj ~/.lein/profiles.clj
 
 
-{:repl {:plugins [[cider/cider-nrepl "0.13.0"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}
+{:repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]}
 
- :user {:plugins [[jonase/eastwood "0.2.3"]] }}
+ ;; The Cider docs say make cider-repl a plugin only for the :repl
+ ;; profile, or else the middleware will run all the time and delay
+ ;; startup.  It says you only need it for :repl.  But that's not
+ ;; really true.  If you want to run the project with "lein run"
+ ;; and connect Cider to it, you need to get the middleware plugged
+ ;; in.  The options are to include it in project.clj, or have it
+ ;; more prominent in profiles.clj.  This seems like a better option.
+ :user {:plugins [[cider/cider-nrepl "0.15.0-snapshot"]
+                  [jonase/eastwood "0.2.3"]]}}
+
+
